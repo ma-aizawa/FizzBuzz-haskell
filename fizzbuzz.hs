ed6ba6b17648d30bf6675c08b64581dd878cc923
Masahiro Aizawa <kingyodojo@gmail.com>
2012-05-08 08:57:20 +0900

    created fizzbuzz by haskell.

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,4 +1,6 @@
-f :: Integer -> [Char]
+main = fizzbuzz [1..100]
+
+f :: Integer -> String
 f x | mod x 15 == 0 = "FizzBuzz"
     | mod x  3 == 0 = "Fizz"
     | mod x  5 == 0 = "Buzz"
@@ -12,3 +14,4 @@ o xs = do
 
 fizzbuzz :: [Integer] -> IO()
 fizzbuzz xs = o $ map f xs
+
